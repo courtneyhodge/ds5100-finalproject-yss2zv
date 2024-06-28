fedc0c846e4845a5eb75c013da9dcd9031a20521
Courtney Hodge <yss2zv@virginia.edu>
2024-06-28 10:47:48 -0400

    modifying project

---
+++ montecarlo/montecarlo.py
@@ -0,0 +1,14 @@
+class Die():
+    def __init__(self):
+        pass
+    
+    
+class Game():
+    def __init__(self):
+        pass
+    
+    
+class Analyzer():
+    def __init__(self):
+        pass
+    
