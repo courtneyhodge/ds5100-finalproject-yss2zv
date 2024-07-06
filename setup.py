c04f7666b579cd85ffbe1d152d4a5c9cfb92804b
Courtney Hodge <yss2zv@virginia.edu>
2024-07-05 23:53:13 -0400

    adding templates

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+
+    name='Montecarlo',
+
+    version='1.0.0',
+
+    url='https://github.com/courtneyhodge/ds5100-finalproject-yss2zv',
+
+    author='Courtney Hodge',
+
+    author_email='yss2zv@virginia/edu',
+
+    description='DS 5100 Final Projecct',
+
+    packages=find_packages(),    
+
+    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+
+)
